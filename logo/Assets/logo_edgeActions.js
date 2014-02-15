22114c39228d78be1520690339a3d86985520e9f
Pinperepette <pinperepette@gmail.com>
2014-02-15 20:33:17 +0100

    Pinperepette
    my web site

---
+++ logo/Assets/logo_edgeActions.js
@@ -0,0 +1,16 @@
+
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",11500,function(sym,e){sym.play("a");});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+
+//=========================================================
+
+//Edge symbol: 'Symbol_1'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",2250,function(sym,e){sym.play("a");});
+//Edge binding end
+})("Symbol_1");
+//Edge symbol end:'Symbol_1'
+})(jQuery,AdobeEdge,"EDGE-34136786");
